869e51a2ba857d9d1505e773e1937da25b3c6948
Maruf Ahmed <marufshmed@gmail.com>
2024-02-02 00:03:30 +0600

    Needed Functions

---
+++ src/bank/BankDetails.java
@@ -0,0 +1,67 @@
+package bank;
+
+import java.util.Scanner;
+
+public class BankDetails {
+	private String accNo;  
+    private String name;  
+    private String accType; 
+    private String creationDate ;
+    private long balance;  
+    private int minBalance = 500 ;
+    static int bankAccountCount=0;
+    
+    Scanner sc = new Scanner(System.in); 
+    
+    public void openAccount() {  
+    	
+    	
+    	
+    }
+    
+    public void showAccount() { 
+    	
+    	
+    }
+    
+    
+    public void deposit() { 
+    	
+    	
+    }
+    
+    public void withdrawal() { 
+    	
+    	
+    }
+    
+    public boolean search(String accNo) { 
+    	
+    	
+    }
+    
+    public boolean update(String ac_no) {
+    	
+    }
+    
+    public boolean delete(String ac_no) { 
+    	
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
